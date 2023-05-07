d4510de2b0a5b0e468b9b4344a3f5ba8e1bbe1aa
io <stuart@kieser.co.za>
2023-05-07 07:47:10 +0200

    Finshed the tutorial

--- get_game_write_along.py
+++ get_game_write_along.py
@@ -5,6 +5,8 @@ from subprocess import PIPE, run  # Allows run of terminal command
 import sys
 
 GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
 
 
 def find_all_game_paths(source):
@@ -17,6 +19,60 @@ def find_all_game_paths(source):
                 path = os.path.join(source, directory)
                 game_paths.append(path)
         break
+    return game_paths
+
+
+def get_name_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+
+    return new_names
+
+
+def copy_ow(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
+
+def make_json_metadata_file(path, game_dirs):
+    data = {"gameNames": game_dirs, "numberOfGames": len(game_dirs)}
+    with open(path, "w") as f:
+        json.dump(data, f)
+
+
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                break
+
+        break
+
+    if code_file_name is None:
+        return
+
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    run_command(command, path)
+
+
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+
+    os.chdir(cwd)
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
 def main(source, target):
@@ -25,7 +81,17 @@ def main(source, target):
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    print(game_paths)
+    new_game_dirs = get_name_paths(game_paths, "_game")
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_ow(src, dest_path)
+        compile_game_code(dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
+
+    create_dir(target_path)
 
 
 if __name__ == "__main__":
